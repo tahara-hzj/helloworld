cf819c276410c83224478315c194a74aa1ccc3a7
LP-Jello <987049974@qq.com>
2018-06-11 15:30:29 +0800

    初次搭建springboot项目

---
+++ src/main/java/com/lanpin/helloworld/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.lanpin.helloworld.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping("/index")
+    @ResponseBody
+    public String showIndex(){
+        return "hello world";
+    }
+
+}
